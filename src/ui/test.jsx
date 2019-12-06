9c6591c6d4e7acfe4e05164ba9d8ad2c70f18aa2
Kiran Surdhar <kiran.surdhar@workday.com>
2019-12-06 10:26:35 -0700

    added quill editor

--- src/ui/test.jsx
+++ src/ui/test.jsx
@@ -1,5 +1,31 @@
 import React from "react"
+import ReactQuill from 'react-quill'
 
-export const Test = () => {
-    return <div>I am a test component</div>
+import 'react-quill/dist/quill.snow.css'
+
+class Quill extends React.Component { // update to use hooks
+  constructor(props) {
+    super(props)
+
+    this.state = { 
+      text: '' 
+    }
+
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  handleChange(value) {
+    this.setState({ text: value })
+  }
+
+  render() {
+    return (
+      <ReactQuill 
+        value={this.state.text}
+        onChange={this.handleChange} 
+      />
+    )
+  }
 }
+
+export default Quill
