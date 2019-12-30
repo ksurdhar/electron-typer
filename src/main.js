91b5e32f702236a6c811e4c7b4636c1f3f20500d
Kiran Surdhar <ksurdhar1@gmail.com>
2019-12-30 11:24:55 -0700

    added electron settings lib for data persistence

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+const settings = require('electron-settings');
 const { app, BrowserWindow } = require('electron');
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
@@ -8,6 +9,13 @@ if (require('electron-squirrel-startup')) { // eslint-disable-line global-requir
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow;
+
+const init = () => {
+  createWindow()
+  // console.log('setting value!')
+  // settings.set('testing.banana', 'hello-world')
+  console.log('getting value', settings.get('testing.banana'))
+}
 
 const createWindow = () => {
   // Create the browser window.
@@ -34,7 +42,7 @@ const createWindow = () => {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow);
+app.on('ready', init);
 
 // Quit when all windows are closed.
 app.on('window-all-closed', () => {
